dfc38fdda68efb6e9a7cb5c1abfe40344699fe6e
Kompactss <rhfprl11@gmail.com>
2022-06-28 06:45:34 +0900

    add unlabeled data

---
+++ wav_preprocessing.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from tqdm.auto import tqdm
+import os
+import librosa
+import numpy as np
+
+CFG = {
+    'SR':16000,
+    'N_MFCC':32, # MFCC 벡터를 추출할 개수
+    'SEED':41
+}
+
+#train_df = pd.read_csv('./train_data.csv')
+#test_df = pd.read_csv('./test_data.csv')
+unlabeled_df = pd.read_csv('./unlabeled_data.csv')
+
+def get_mfcc_feature(df, data_type, save_path):
+    # Data Folder path
+    root_folder = './wav_dataset'
+    if os.path.exists(save_path):
+        print(f'{save_path} is exist.')
+        return
+    features = []
+    for uid in tqdm(df['id']):
+        root_path = os.path.join(root_folder, data_type)
+        path = os.path.join(root_path, str(uid).zfill(5)+'.wav')
+
+        # librosa패키지를 사용하여 wav 파일 load
+        y, sr = librosa.load(path, sr=CFG['SR'])
+        
+        # librosa패키지를 사용하여 mfcc 추출
+        mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=CFG['N_MFCC'])
+
+        y_feature = []
+        # 추출된 MFCC들의 평균을 Feature로 사용
+        for e in mfcc:
+            y_feature.append(np.mean(e))
+        features.append(y_feature)
+    
+    # 기존의 자가진단 정보를 담은 데이터프레임에 추출된 오디오 Feature를 추가
+    mfcc_df = pd.DataFrame(features, columns=['mfcc_'+str(x) for x in range(1,CFG['N_MFCC']+1)])
+    df = pd.concat([df, mfcc_df], axis=1)
+    df.to_csv(save_path, index=False)
+    print('Done.')
+
+#get_mfcc_feature(train_df, 'train', './train_mfcc_data.csv')
+#get_mfcc_feature(test_df, 'test', './test_mfcc_data.csv')
+get_mfcc_feature(unlabeled_df, 'unlabeled', './unlabeled_mfcc_data.csv')
